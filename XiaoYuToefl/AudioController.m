49614fde75b8f6e7bcf88eeeedf3cbc179c22286
XingyinZhu <xingyinzhu@gmail.com>
2013-08-24 15:00:42 +0800

    v 0.52 -- fix a bug …

--- XiaoYuToefl/AudioController.m
+++ XiaoYuToefl/AudioController.m
@@ -16,28 +16,29 @@
 
 -(void)preloadAudioEffects:(NSArray*)effectFileNames
 {
-  //initialize the effects array
-  audio = [NSMutableDictionary dictionaryWithCapacity: effectFileNames.count];
-  
-  //loop over the filenames
-  for (NSString* effect in effectFileNames) {
+    //initialize the effects array
+    if (audio == Nil)
+    {
+        audio = [[NSMutableDictionary alloc]init];
+    }
+
+    //loop over the filenames
+    for (NSString* effect in effectFileNames)
+    {
+        //1 get the file path URL
+        NSString* soundPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent: effect];
+        NSURL* soundURL = [NSURL fileURLWithPath: soundPath];        //2 load the file contents
+        NSError* loadError = nil;
+        AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error: &loadError];
+        NSAssert(loadError==nil, @"load sound failed");
+
+        //3 prepare the play
+        player.numberOfLoops = 0;
+        [player prepareToPlay];
     
-    //1 get the file path URL
-    NSString* soundPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent: effect];
-    NSURL* soundURL = [NSURL fileURLWithPath: soundPath];
-    
-    //2 load the file contents
-    NSError* loadError = nil;
-    AVAudioPlayer *player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundURL error: &loadError];
-    NSAssert(loadError==nil, @"load sound failed");
-    
-    //3 prepare the play
-    player.numberOfLoops = 0;
-    [player prepareToPlay];
-    
-    //4 add to the array ivar
-    audio[effect] = player;
-  }
+        //4 add to the array ivar
+        audio[effect] = player;
+    }
 }
 
 -(void)playEffect:(NSString*)name
